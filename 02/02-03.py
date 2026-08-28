83df567609afd958b578cdf6fdfad7840b7d8967
Carlos J. Garcia <your_email@example.com>
2026-08-28 11:21:17 +0200

    02-03

--- 02/02-03.py
+++ 02/02-03.py
@@ -24,8 +24,11 @@ response = client.chat.completions.create(model=MODEL_NAME, messages=messages)
 clean_response = response.choices[0].message.content.strip()                      # Remove trailing \n in the LLM response
 messages.append({"role": "assistant", "content": clean_response})                 # Add the reply to the list. Use the role (dictionary key) "assistant"
 
+print()
 print("Interaction #1, context provided")
 print(clean_response)
+print(f"Tokens: {response.usage.total_tokens} (Total) = {response.usage.prompt_tokens} (Prompt, including 'messages' list) + {response.usage.completion_tokens} (Completion, this reply including reasoning)")
+print()
 
 
 # Interaction #2
@@ -36,5 +39,5 @@ messages.append({"role": "assistant", "content": clean_response})               
 
 print("Interaction #2, is there context (state) still there? Yes, because I pass all my previous user propmts together every time")
 print(clean_response)
-
+print(f"Tokens: {response.usage.total_tokens} (Total) = {response.usage.prompt_tokens} (Prompt, including 'messages' list) + {response.usage.completion_tokens} (Completion, this reply including reasoning)")
 print()
