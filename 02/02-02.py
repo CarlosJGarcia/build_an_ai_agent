9d5c1848b46b16aeca34e4d2228bf53267d5f27f
Carlos J. Garcia <your_email@example.com>
2026-08-28 09:19:57 +0200

    02-02

---
+++ 02/02-02.py
@@ -0,0 +1,37 @@
+# OpenAI’s Chat Completions API 
+# Demostrating that LLMs are stateless
+
+import os
+from openai import OpenAI
+
+vllm_server_fqdn = os.getenv("VLLM_SERVER_FQDN")
+if not vllm_server_fqdn:
+    raise ValueError("ERROR: VLLM_SERVER_FQDN environment variable is not set.")
+vllm_url = f"http://{vllm_server_fqdn}:8000/v1"
+MODEL_NAME = "nvidia/Qwen3.6-35B-A3B-NVFP4"
+
+client = OpenAI(base_url=vllm_url, api_key="EMPTY") 
+
+response = client.chat.completions.create(
+    model=MODEL_NAME,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},   # How the conversational, instruction-tuned LLM should answer
+        {"role": "user", "content": "My name is Carlos"},                # Context provided by the user
+        {"role": "user", "content": "What is my name?"}                  # The specific question to be answered
+    ]
+)
+
+print("Interaction #1, context provided")
+print(response.choices[0].message.content)
+
+response = client.chat.completions.create(
+    model=MODEL_NAME,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},   # How the conversational, instruction-tuned LLM should answer
+        {"role": "user", "content": "What is my name?"}                  # The specific question to be answered
+    ]
+)
+
+print("Interaction #2, is there context (state) still there?")
+print(response.choices[0].message.content)
+print()
