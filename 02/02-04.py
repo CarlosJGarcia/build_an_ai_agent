ba340e6d62cd714a5d866c388d435665de524b1d
Carlos J. Garcia <your_email@example.com>
2026-08-29 06:07:33 +0200

    02-04

--- 02/02-04.py
+++ 02/02-04.py
@@ -1,14 +1,13 @@
 # OpenAI’s Chat Completions API 
-# Managing conversation history
-# Demonstrating how to maintain state by passing previous history
-# By maintaining a 'messages' list and appending the LLM's replies to it after each interaction, we pass the entire conversation history back to the model with every new request, giving it the "memory" it needs.
-# User messages are added with the user role (dictionary key) and model responses are added with the assistant role (dictionary key)
+# Structured data extraction
+# Demonstrating how to 'convince' a LLM to reply using data structures (JSON)
+# By swiching from "conversation mode" to "data structures" we bridge the gap between a "conversational brain" and the strict requirements of APIs and tools
 
 import os
 from openai import OpenAI
 from pydantic import BaseModel
 
-class ExtractedInfo(BaseModel):      # Define data structure named ExtractedInfo using pydantic model
+class ExtractedInfo(BaseModel):      # Define data structure named ExtractedInfo using pydantic 
     name: str                        # Required string
     email: str                       # Required string
     phone: str | None = None         # Optonal string (it can be either a string or nothing, by default it's nothing) 
@@ -22,7 +21,7 @@ MODEL_NAME = "nvidia/Qwen3.6-35B-A3B-NVFP4"
 client = OpenAI(base_url=vllm_url, api_key="EMPTY") 
 
 
-# En lugar de client.chat.completions.create() ahora uso client.beta.chat.completions.parse()
+# Instead of client.chat.completions.create() now we invoke the LLM with client.beta.chat.completions.parse() and parameter response_format
 # OpenAI's GPT-4 has been heavily fine-tuned to recognize the structured output request and completely suppress their conversational instincts.
 # Qwen, open-source running locally often needs an explicit reminder to just output JSON
 # I need to add a system message to explicitly instruct the model to act as a strict data parser and suppress all conversation
@@ -34,7 +33,7 @@ response = client.beta.chat.completions.parse(
     response_format=ExtractedInfo
 )
 
-# En lugar de message.content, recogemos el resultado de message.parsed
+# Instead of using message.content, we get the LLM response using message.parsed
 clean_response = response.choices[0].message.parsed
 
 print()
