2e002850149b982ebd978991f4ab85d17fc9833b
Carlos Garcia <cgarciams@hotmail.com>
2026-09-24 11:12:35 +0200

    02-05-03

--- 02/02-05-03.py
+++ 02/02-05-03.py
@@ -3,13 +3,14 @@
 # AsyncOpenAI -> Concurrent Chat Completion
 # Asyncio -> Concurrency framework for I/O-bound tasks like DB queries or LLM inference
 # Rich Progress -> Real-time progress tracking
+# The progress bar is very nice but it adds another layer of complexity to the code
 
 import os
 import time
 import asyncio
 from openai import AsyncOpenAI
 from rich.console import Console
-from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.progress import Progress
 
 console = Console()
 
@@ -22,7 +23,7 @@ MODEL_TEMPERATURE = 0.0
 MODEL_NAME = "nvidia/Qwen3.6-35B-A3B-NVFP4"
 SYSTEM_PROMPT = "You are a helpful assistant. Output plain text only. Do not use emojis or emoticons. "
 
-TOTAL_QUESTIONS = 50          # Limit to 50 requests
+TOTAL_QUESTIONS = 15          # Limit to 50 requests
 CONCURRENT_QUESTIONS = 10     # Limit to 10 concurrent requests
 
 # Initialize the semaphore and the async client
@@ -47,24 +48,33 @@ async def main():
     console.print(f"\nAsking {TOTAL_QUESTIONS} questions concurrently with a concurrency limit of {CONCURRENT_QUESTIONS} at a time", style="gold1", highlight=False)
 
     # Execute all coroutines concurrently with a progress bar
+    # "with": Context manager pattern
+    # Progress() creates a progress bar instance
     with Progress() as progress:
-        progress_task = progress.add_task(f"[cyan]Processing {TOTAL_QUESTIONS} questions...", total=TOTAL_QUESTIONS)
+        progress_task = progress.add_task(f"Asking {MODEL_NAME}:", total=TOTAL_QUESTIONS)
 
+        # This is defined within the scope of with, so it can modify progress (the progress bar instance)
         async def tracked_inference(prompt: str):
             result = await inference(prompt)
             progress.update(progress_task, advance=1)
             return result
 
+        # Create a list with all the coroutine calls, each one with a different question as parameter
         tasks = [tracked_inference(p) for p in prompts]
+
+        # Execute all the coroutines concurrently and wait until all are completed
         results = await asyncio.gather(*tasks, return_exceptions=True)
 
     end_time = time.time()
     execution_time_minutes = (end_time - start_time) / 60
 
     # Show each question and the result
+    console.print(f"\nAll questions and answers:", style="gold1", highlight=False)
+    n = 1
     for prompt, result in zip(prompts, results):
-        console.print(f"Q: {prompt}", style="white", highlight=False)
-        print(f"A: {result}\n")
+        console.print(f"Q #{n}: {prompt}", style="white", highlight=False)
+        print(f"A #{n}: {result}\n")
+        n += 1
     console.print(f"Time: {execution_time_minutes:.2f} minutes\n", style="cyan", highlight=False)
   
 
